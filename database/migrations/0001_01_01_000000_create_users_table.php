a3a7f2170c30e72ded30270b1b4f9b18f597d337
Cesar Pinero <cpinero522003@gmail.com>
2025-09-05 19:20:45 +0800

    updated migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->enum('role', ['cashier', 'admin', 'customer'])->default('customer');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
