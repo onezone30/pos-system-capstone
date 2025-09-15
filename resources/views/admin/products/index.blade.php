7c4bccf1c5d5b50b38108d52b8b8f717936ae180
Cesar Pinero <cpinero522003@gmail.com>
2025-09-16 01:05:39 +0800

    added view for products, can edit and delete products

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,8 +1,4 @@
-<x-layout>
-
-    <x-sidebar :role="$user->role"/>
-
-    <main class="p-4 sm:ml-64 mt-14">
+<x-main :user="$user">
 
         <div class="flex justify-between items-center">
 
@@ -11,26 +7,161 @@
                 Products
             </h1>
             
-            <form action="">
-
+            <x-forms.form id="create-product-form" action="{{ route('admin.products.store') }}" method="POST">
                 
-
-                <x-button>
+                <x-button data-modal-target="crud-modal" data-modal-toggle="crud-modal">
                     Add Product
                 </x-button>
-            </form>
+
+                <!-- add modal -->
+                <x-modals.create header="Create Product">
+
+                    <div class="space-y-6">
+                        <x-forms.input 
+                            name="name"
+                            label="Name"
+                            placeholder="Enter name of product"/>
+
+                        <x-forms.select label="Select category" name="category">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">
+                                    {{ $category->name }}
+                                </option>
+                            @endforeach
+                        </x-forms.select>
+
+                        <h1 class="font-bold text-base">Small Size</h1>
+                        <div class="grid grid-cols-2 gap-4">
+                            <input type="hidden" value="small" name="size[]">
+
+                            <x-forms.input name="price[]" placeholder="Enter price"/>
+                            <x-forms.input name="quantity_stock[]" placeholder="Enter quantity stock"/>
+                        </div>
+
+                        <h1 class="font-bold text-base">Medium Size</h1>
+                        <div class="grid grid-cols-2 gap-4">
+                            <input type="hidden" value="medium" name="size[]">
+
+                            <x-forms.input name="price[]" placeholder="Enter price"/>
+                            <x-forms.input name="quantity_stock[]" placeholder="Enter quantity stock"/>
+                        </div>
+
+                        <h1 class="font-bold text-base">Large Size</h1>
+                        <div class="grid grid-cols-2 gap-4">
+                            <input type="hidden" value="large" name="size[]">
+
+                            <x-forms.input name="price[]" placeholder="Enter price"/>
+                            <x-forms.input name="quantity_stock[]" placeholder="Enter quantity stock"/>
+                        </div>
+
+                        <x-slot name="footer">
+                            <x-forms.button>
+                                Create Product
+                            </x-forms.button>
+                        </x-slot>
+                    </div>
+
+                    
+
+                </x-modals.create>
+
+            </x-forms.form>
 
             
 
         </div>
-        <div class="mt-5 grid grid-cols-4 gap-y-4 gap-x-2">
+        
+        <div
+            id="product-list" 
+            class="mt-5 grid grid-cols-1 gap-y-4 gap-x-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 
             @foreach ($products as $product)
-                <x-product-card :product="$product"/>
+                <x-product-card :product="$product" :categories="$categories"/>
+                <x-modals.delete
+                    :title="$product->name"
+                    id="{{ $product->id }}" 
+                    :action="route('admin.products.destroy', $product->id)"/>
+                <x-modals.edit 
+                    id="{{ $product->id }}"
+                    header="Edit product: {{ $product->name }}"
+                    :action="route('admin.products.update', $product->id)">
+
+
+                    <div class="space-y-6">
+                        <x-forms.input 
+                            name="name"
+                            label="Name"
+                            placeholder="Enter name of product"
+                            value="{{ $product->name }}"/>
+
+                        <x-forms.select label="Select category" name="category">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}" {{ $product->category_id == $category->id ? 'selected' : '' }}>
+                                    {{ $category->name }} 
+                                </option>
+                            @endforeach
+                        </x-forms.select>
+
+                        <h1 class="font-bold text-base">Small Size</h1>
+                        <div class="grid grid-cols-2 gap-4">
+                            <input type="hidden" value="Small" name="size[]">
+                            <x-forms.input 
+                                label="Price" 
+                                name="price[]" 
+                                placeholder="Enter price" 
+                                value="{{ optional($product->prices->where('size', 'small')->first())->price }}"/>
+
+                            <x-forms.input 
+                                label="Quantities" 
+                                name="quantity_stock[]" 
+                                placeholder="Enter quantity stock" 
+                                value="{{ optional($product->prices->where('size', 'small')->first())->quantity_stock }}"/>
+                        </div>
+
+                        <h1 class="font-bold text-base">Medium Size</h1>
+                        <div class="grid grid-cols-2 gap-4">
+                            <input type="hidden" value="Medium" name="size[]">
+                            <x-forms.input 
+                                label="Price" 
+                                name="price[]" 
+                                placeholder="Enter price" 
+                                value="{{ optional($product->prices->where('size', 'medium')->first())->price }}"/>
+                            <x-forms.input 
+                                label="Quantities" 
+                                name="quantity_stock[]" 
+                                placeholder="Enter quantity stock" 
+                                value="{{ optional($product->prices->where('size', 'medium')->first())->quantity_stock }}"/>
+                        </div>
+
+                        <h1 class="font-bold text-base">Large Size</h1>
+                        <div class="grid grid-cols-2 gap-4">
+                            <input type="hidden" value="Large" name="size[]">
+                            <x-forms.input 
+                                label="Price" 
+                                name="price[]" 
+                                placeholder="Enter price" 
+                                value="{{ optional($product->prices->where('size', 'large')->first())->price }}"/>
+                            <x-forms.input 
+                                label="Quantities" 
+                                name="quantity_stock[]" 
+                                placeholder="Enter quantity stock" 
+                                value="{{ optional($product->prices->where('size', 'large')->first())->quantity_stock }}"/>
+                        </div>
+
+
+                    </div>
+                    <x-slot name="footer">
+                        <x-forms.button>
+                            Update Product
+                        </x-forms.button>
+                    </x-slot>
+                
+                </x-modals.edit>
             @endforeach
 
         </div>
 
-    </main>
 
-</x-layout>
+</x-main>
+
+
