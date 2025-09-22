96df4e72598e6caafdbc8ba39fe2bca8d816bb51
Cesar Pinero <cpinero522003@gmail.com>
2025-09-23 02:44:19 +0800

    fixed the alpine problem / changed it to vanilla js

--- resources/views/components/forms/password.blade.php
+++ resources/views/components/forms/password.blade.php
@@ -6,20 +6,41 @@
 ])
 
 <x-forms.field :label="$label" :name="$name">
-    <div x-data="{ show: false }" class="relative w-full">
+    <div class="relative w-full">
         <input
-            :type="show ? 'text' : 'password'"
+            type="password"
+            id="password-input-{{ $name }}"
             name="{{ $name }}"
-            class="{{ $class }} pr-12" {{-- padding-right for button space --}}
+            class="{{ $class }} pr-12"
             placeholder="{{ $placeholder }}"
         />
 
         <!-- Toggle button -->
         <button type="button"
-            @click="show = !show"
+            onclick="togglePassword('{{ $name }}')"
             class="absolute inset-y-0 right-0 px-3 flex items-center text-gray-600 hover:text-gray-900">
-            <span x-show="!show">👁️</span>
-            <span x-show="show">🙈</span>
+            <span id="show-icon-{{ $name }}">👁️</span>
+            <span id="hide-icon-{{ $name }}" style="display:none;">🙈</span>
         </button>
     </div>
 </x-forms.field>
+
+@once
+    <script>
+        function togglePassword(name) {
+            const input = document.getElementById(`password-input-${name}`);
+            const showIcon = document.getElementById(`show-icon-${name}`);
+            const hideIcon = document.getElementById(`hide-icon-${name}`);
+
+            if (input.type === "password") {
+                input.type = "text";
+                showIcon.style.display = "none";
+                hideIcon.style.display = "inline";
+            } else {
+                input.type = "password";
+                showIcon.style.display = "inline";
+                hideIcon.style.display = "none";
+            }
+        }
+    </script>
+@endonce
