c9cb58aa99b4a2c81a578c69c94258125ad5093e
Cesar Pinero <cpinero522003@gmail.com>
2025-10-04 03:07:27 +0800

    can now create/edit/delete product dynamically

--- resources/views/livewire/product/create-product-form.blade.php
+++ resources/views/livewire/product/create-product-form.blade.php
@@ -1,46 +1,66 @@
-<form wire:submit.prevent="create">
-    <div class="space-y-6">
+<div 
+    x-data="{ open: false }"
+    x-show="open"
+    x-cloak
+    x-transition:enter="transition ease-out duration-500"
+    x-transition:enter-start="opacity-0 -translate-y-10"
+    x-transition:enter-end="opacity-100 translate-y-0"
+    x-transition:leave="transition ease-in duration-300"
+    x-transition:leave-start="opacity-100 translate-y-0"
+    x-transition:leave-end="opacity-0 -translate-y-10"
+    x-on:open-create-modal.window="open = true"
+    x-on:close-create-modal.window="open = false"
+>
 
-        <!-- Names -->
-        <x-forms.input 
-            wire:model="name"
-            label="Name"
-            placeholder="Enter name of product"/>
+    <form wire:submit.prevent="create">
+        <div class="space-y-6">
 
-        <!-- Categories -->
-        <x-forms.select label="Select category" wire:model="category_id">
-            <option value="">-- Select category --</option>
-            @foreach ($categories as $category)
-                <option value="{{ $category->id }}">
-                    {{ $category->name }}
-                </option>
+            <!-- Names -->
+            <x-forms.input 
+                wire:model="name"
+                label="Name"
+                placeholder="Enter name of product"/>
+
+            <!-- Categories -->
+            <x-forms.select label="Select category" wire:model="category_id">
+                <option value="">-- Select category --</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">
+                        {{ $category->name }}
+                    </option>
+                @endforeach
+            </x-forms.select>
+
+            <!-- Sizes -->
+            @foreach ($sizes as $index => $size)
+                <h1 class="font-bold text-base">
+                    {{ ucfirst($size) }} Size
+                </h1>
+                <div class="grid grid-cols-2 gap-4">
+                    {{-- Price --}}
+                    <x-forms.input 
+                        wire:model="prices.{{ $index }}"
+                        placeholder="Enter price"/>
+                    {{-- Quantity --}}
+                    <x-forms.input 
+                        wire:model="quantities.{{ $index }}"
+                        placeholder="Enter quantity stock"/>
+                </div>
             @endforeach
-        </x-forms.select>
 
-        <!-- Sizes -->
-        @foreach ($sizes as $index => $size)
-            <h1 class="font-bold text-base">
-                {{ ucfirst($size) }} Size
-            </h1>
-            <div class="grid grid-cols-2 gap-4">
-                {{-- Price --}}
-                <x-forms.input 
-                    wire:model="prices.{{ $index }}"
-                    placeholder="Enter price"/>
-                {{-- Quantity --}}
-                <x-forms.input 
-                    wire:model="quantities.{{ $index }}"
-                    placeholder="Enter quantity stock"/>
+            <x-forms.input 
+                wire:model="product_image"
+                label="Product Image" 
+                type="file"/>
+
+            <!-- Create Button -->
+            <div class="flex justify-end mt-4">
+                <x-forms.button>
+                    Create Product
+                </x-forms.button>
             </div>
-        @endforeach
-
-        <!-- Create Button -->
-        <div class="flex justify-end mt-4">
-            <x-forms.button>
-                Create Product
-            </x-forms.button>
         </div>
-    </div>
-</form>
+    </form>
 
 
+</div>
--- resources/views/livewire/product/product-list.blade.php
+++ resources/views/livewire/product/product-list.blade.php
@@ -1,27 +1,35 @@
-<div>
+<div
+    id="product-list" 
+    class="mt-5 grid grid-cols-1 gap-y-4 gap-x-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+
+    @foreach ($products as $product)
     <div
-        id="product-list" 
-        class="mt-5 grid grid-cols-1 gap-y-4 gap-x-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-
-        @foreach ($products as $product)
-            <x-product-card 
-                :product="$product" 
-                :categories="$categories"
-                wire:key="product-card-{{ $product->id }}"/>
-
-            <!-- Edit Modal -->
-            <x-modals.edit 
-                id="{{ $product->id }}" 
-                header="Edit {{ $product->name }}"
-                wire:key="delete-modal-{{ $product->id }}"
-                >
-                @livewire('product.edit-product-form', ['product' => $product])
-            </x-modals.edit>
-        @endforeach
-
-        <!-- Delete Modal -->
-        <x-modals.delete action="delete" />
-
+        wire:key="product-card-{{ $product->id }}"
+        x-data="{ show: false }"
+        x-init="setTimeout(() => show = true, 200)"
+        x-show="show"
+        x-transition:enter="transition ease-out duration-500"
+        x-transition:enter-start="opacity-0 -translate-y-10"
+        x-transition:leave="transition ease-in duration-300"
+        x-transition:enter-end="opacity-100 translate-y-0"
+        x-transition:leave-start="opacity-100 translate-y-0"
+        x-transition:leave-end="opacity-0 -translate-y-10"                     
+        >
+        <x-product-card 
+            :product="$product" 
+            :categories="$categories" />
     </div>
 
+    @endforeach
+
+
+    <!-- rendering modals -->
+    <!-- Delete Modal -->
+    <x-modals.delete action="delete" />
+    <!-- Edit Modal -->
+    <x-modals.edit action="edit">
+        <livewire:product.edit-product-form/>
+    </x-modals.edit>
+
 </div>
+
