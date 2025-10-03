0138fd0b55622118354914f7a497bf07647b0864
Cesar Pinero <cpinero522003@gmail.com>
2025-10-04 03:04:25 +0800

    made the delete modal flexible

--- resources/views/components/modals/delete.blade.php
+++ resources/views/components/modals/delete.blade.php
@@ -1,40 +1,62 @@
-@props(['id', 'action', 'title' => false])
+<div 
+    x-data="{ open: false, id: null, name: '' }"
+    x-on:open-delete-modal.window="
+        id = $event.detail.id;
+        name = $event.detail.name;
+        open = true
+    "
+    x-on:close-delete-modal.window="open = false"
+    x-show="open"
+    x-transition
+    x-cloak
+    wire:ignore.self
+    x-on:click="open = false"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
 
+    <div class="bg-white dark:bg-gray-700 rounded-lg shadow-md w-full max-w-md p-6">
+        <!-- Close Button -->
+        <button 
+            type="button"
+            class="absolute top-3 right-3 text-gray-400 hover:text-gray-900 dark:hover:text-white"
+            @click="open = false"
+        >
+            ✕
+        </button>
 
-<div id="delete-modal-{{ $id }}" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
-            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="delete-modal-{{ $id }}">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                </svg>
-                <span class="sr-only">Close modal</span>
+        <div class="text-center">
+
+            <!-- Icon -->
+            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" 
+                 fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" 
+                      stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+            </svg>
+
+            <h3 class="text-lg font-normal text-gray-500 dark:text-gray-400">
+                Are you sure you want to delete <b x-text="name"></b>?
+            </h3>
+            <p class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                This action <b>cannot</b> be undone.
+            </p>
+
+            <button 
+                type="submit"
+                @click="$wire.delete(id)"
+                class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none 
+                        focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm 
+                        px-5 py-2.5">
+                Yes, delete it
             </button>
-            <div class="p-4 md:p-5 text-center">
-                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                </svg>
-                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete {{ $title }}?</h3>
-                <div class="flex justify-center">
-                    <x-forms.form method="POST" action="{{ $action }}">
 
-                        @method('DELETE')
+            <button 
+                type="button"
+                @click="open = false"
+                class="px-5 py-2.5 text-sm font-medium text-gray-900 bg-white border rounded-lg 
+                        hover:bg-gray-100 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 
+                        dark:hover:text-white dark:hover:bg-gray-700">
+                Cancel
+            </button>
 
-                        <button data-modal-hide="delete-modal-{{ $id }}" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                            Yes, I'm sure
-                        </button>
-
-                    </x-forms.form>
-
-
-                    <button data-modal-hide="delete-modal-{{ $id }}" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                        No, cancel
-                    </button>
-                </div>
-
-
-                
-            </div>
         </div>
     </div>
 </div>
