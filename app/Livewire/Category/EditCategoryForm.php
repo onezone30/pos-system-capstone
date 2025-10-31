0f75a4bfd66293aa139f31749636c66e3a1c3f42
Cesar Pinero <cpinero522003@gmail.com>
2025-11-01 04:57:22 +0800

    done with the edit category

--- app/Livewire/Category/EditCategoryForm.php
+++ app/Livewire/Category/EditCategoryForm.php
@@ -2,10 +2,53 @@
 
 namespace App\Livewire\Category;
 
+use App\Models\Category;
+use App\Services\CategoryServices;
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class EditCategoryForm extends Component
 {
+    public Category $category;
+    public $name = '';
+    public $description = '';
+
+    public function rules()
+    {
+        return [
+            'name' => ['string', 'required', 'max:255', Rule::unique('categories', 'name')->ignore($this->category->id)],
+            'description' => ['string', 'nullable', 'max:255'],
+        ];
+    }  
+
+    #[On('open-edit-modal')]
+    public function load(int $id)
+    {
+        $this->category = Category::findOrFail($id);
+
+        $this->name = $this->category->name;
+        $this->description = $this->category->description;
+    }
+
+    public function update(CategoryServices $services)
+    {
+        $this->validate();
+
+        $categoryData = [
+            'name' => $this->name,
+            'description' => $this->description,
+        ];
+
+        if(!$services->update($this->category, $categoryData)) {
+            $this->dispatch('toast.error', message: 'Error updating category');
+        }
+
+        $this->dispatch('editCategory');
+        $this->dispatch('toast.success', message: "{$this->name} category has been create");
+        $this->dispatch('close-edit-modal');
+    }
+
     public function render()
     {
         return view('livewire.category.edit-category-form');
--- resources/views/livewire/category/edit-category-form.blade.php
+++ resources/views/livewire/category/edit-category-form.blade.php
@@ -1,3 +1,38 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+<div 
+    x-data="{ open: false }"
+    x-show="open"
+    x-cloak
+    x-transition:enter="transition ease-out duration-500"
+    x-transition:enter-start="opacity-0 -translate-y-10"
+    x-transition:enter-end="opacity-100 translate-y-0"
+    x-transition:leave="transition ease-in duration-300"
+    x-transition:leave-start="opacity-100 translate-y-0"
+    x-transition:leave-end="opacity-0 -translate-y-10"
+    x-on:open-edit-modal.window="open = true"
+    x-on:close-edit-modal.window="open = false"
+>
+
+    <form wire:submit.prevent="update">
+
+        <div class="space-y-6">
+
+            <!-- Names -->
+            <x-forms.input 
+                wire:model="name"
+                label="Name"
+                placeholder="Enter name of product"/>
+
+            <x-forms.textarea 
+                wire:model="description"
+                label="Description" 
+                placeholder="Enter description"/>
+
+            <!-- Edit Button -->
+            <div class="flex justify-end mt-4">
+                 <x-button size="2xl" color="blue" wire:click="update" wire:target="update">
+                    Update Product
+                </x-button>
+            </div>
+        </div>
+    </form>
 </div>
