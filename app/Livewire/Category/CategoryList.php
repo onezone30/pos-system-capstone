185571cd04f5820d6bfc74e769bfebec541ef67e
Cesar Pinero <cpinero522003@gmail.com>
2025-11-01 04:58:26 +0800

    done with the category display

--- app/Livewire/Category/CategoryList.php
+++ app/Livewire/Category/CategoryList.php
@@ -7,22 +7,59 @@ use Livewire\Component;
 
 class CategoryList extends Component
 {
+    public $category = [];
+    public string $search = '';
+
     protected $listeners = [
-        'createCategory' => '$refresh',
-        'deleteCategory' => '$refresh',
+        'createCategory' => 'load',
+        'editCategory' => 'load',
+        'searchUpdated' => 'categorySearch'
     ];
+
+    public function mount()
+    {
+        $this->load();
+    }
+
+    public function load()
+    {
+        $this->category = $this->filteredCategory();
+    }
+
+    public function categorySearch($search)
+    {
+        $this->search = trim($search);
+        $this->load();
+    }
+
+    public function filteredCategory()
+    {
+        $categories = Category::query()
+            ->when($this->search, function($query) {
+                $query->where('name', 'like',"%{$this->search}%");
+            })
+            ->latest()
+            ->get();
+
+        return $categories;
+    }
 
     public function delete(int $id)
     {
-        Category::findOrFail($id)->delete();
+        $category = Category::findOrFail($id);
+        
+        if(! $category->delete()) {
+            $this->dispatch('toast.success', message: "{$category->name}");
+        }
 
         $this->dispatch('deleteCategory');
         $this->dispatch('close-delete-modal');
+        $this->dispatch('toast.success', message: "{$category->name} has been deleted");
     }
 
     public function render()
     {
-        $categories = Category::all();
+        $categories = $this->filteredCategory();
 
         return view('livewire.category.category-list', [
             'categories' => $categories
