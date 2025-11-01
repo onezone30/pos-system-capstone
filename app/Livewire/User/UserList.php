76d9ab877f8dd093ac329a783e6fafeb32f10148
Cesar Pinero <cpinero522003@gmail.com>
2025-11-02 03:04:13 +0800

    update: refactor the delete function and load function, fixed the grid in the view

--- app/Livewire/User/UserList.php
+++ app/Livewire/User/UserList.php
@@ -11,28 +11,31 @@ use function PHPSTORM_META\map;
 class UserList extends Component
 {
     public string $search = "";
+    public $users = [];
 
     protected $listeners = [
-        'deleteUser' => '$refresh',
-        'createUser' => '$refresh',
-        'editUser' => '$refresh',
         'searchUpdated' => 'userSearch',
+        'editUser' => 'load',
+        'createUser' => 'load'
     ];
+
+    public function mount()
+    {
+        $this->load();
+    }
+
+    public function load()
+    {
+        $this->users = $this->filteredUser();
+    }
 
     public function userSearch($search)
     {
         $this->search = trim($search);
+        $this->load();
     }
 
-    public function delete(int $id)
-    {
-        User::findOrFail($id)->delete();
-
-        $this->dispatch('deleteUser');
-        $this->dispatch('close-delete-modal');
-    }
-
-    public function render()
+    public function filteredUser()
     {
         $users = User::query()
             ->when($this->search, function($query) {
@@ -43,8 +46,26 @@ class UserList extends Component
             ->latest()
             ->get();
 
+        return $users;
+    }
+
+    public function delete(int $id)
+    {
+        $user = User::findOrFail($id);
+        
+        if(! $user->delete()) {
+            $this->dispatch('toast.success', message: "Failed to delete {$user->name}");
+        }
+        
+        $this->load();
+        $this->dispatch('toast.success', message: "{$user->name} has been deleted");
+        $this->dispatch('close-delete-modal');
+    }
+
+    public function render()
+    {
         return view('livewire.user.user-list', [
-            'users' => $users
+            'users' => $this->users,
         ]);
     }
 }
--- resources/views/livewire/user/user-list.blade.php
+++ resources/views/livewire/user/user-list.blade.php
@@ -1,6 +1,6 @@
 <div
     id="user-list" 
-    class="mt-5 grid grid-cols-1 gap-y-4 gap-x-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-stretch">
+    class="mt-5 grid grid-cols-1 gap-y-4 gap-x-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-stretch">
 
     @foreach ($users as $user)
         <x-user-card :card_user="$user" :key="$user->id"/>    
@@ -8,12 +8,12 @@
 
     <!-- rendering modals -->
     <!-- Edit Modal -->
-    <x-modals.edit action="edit">
+    <x-modals.edit>
         <livewire:user.edit-user-form />
     </x-modals.edit>
     <!-- View Modal -->
     <x-modals.view-user />
     <!-- Delete Modal -->
-    <x-modals.delete action="delete" />
+    <x-modals.delete />
 </div>
 
