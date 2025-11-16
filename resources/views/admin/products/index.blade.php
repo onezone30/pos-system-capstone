5bc7b7f4a7609e39bb92ee390377a6c635051e54
Cesar Pinero <cpinero522003@gmail.com>
2025-11-16 14:35:57 +0800

    update: added sorting using alpine

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -3,7 +3,9 @@
         <x-page-title text="Products" />
 
         <div
-            x-data="{ search: '' }" 
+            x-data="{ 
+                search: '', 
+            }" 
             class="mt-4 flex justify-between items-center">
 
             <input
@@ -21,6 +23,56 @@
                 <livewire:product.create-product-form />  
             </x-modals.create>
 
+        </div>
+
+        <div
+            x-data="{ 
+                category: '',
+                order: '',
+                stock: '',
+  
+                updateFilters() {
+                    $dispatch('product-filter', {
+                        category: this.category,
+                        order: this.order,
+                        stock: this.stock
+                    });
+                },
+            }"
+            class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 "
+        >
+
+            <x-forms.select 
+                x-model="category" 
+                @change="updateFilters()"
+            >
+                <option value="">All Categories</option>
+                @foreach ($categories as $category)
+                <option value="{{ $category->name }}">
+                    {{ $category->name }}
+                </option>
+                @endforeach
+            </x-forms.select>
+
+            <x-forms.select 
+                x-model="order" 
+                @change="updateFilters()"
+            >
+                <option value="">Default</option>
+                <option value="asc">Ascending (A → Z)</option>
+                <option value="desc">Descending (Z → A)</option>
+            </x-forms.select>
+
+            <x-forms.select 
+                x-model="stock" 
+                @change="updateFilters()"
+            >
+                <option value="">All Stocks</option>
+                <option value="low">Low Stock (0–10)</option>
+                <option value="medium">Medium Stock (11–50)</option>
+                <option value="high">High Stock (51+)</option>
+            </x-forms.select>
+            
         </div>
         
         <x-section>
